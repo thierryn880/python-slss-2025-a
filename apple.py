37a8c217e829c8f33a8d3b8381d3a7b61317d263
TNTbro35 <thierryoma@icloud.com>
2025-10-02 14:39:22 -0700

    second commit - added nots and apple

---
+++ apple.py
@@ -0,0 +1,44 @@
+# notes - introduction
+# 16 september
+# Thierry Narcisse
+
+# Create our owm chatbot
+
+# 1. greet the user with apredeterminted statement
+# choose your own adventure
+# Umar Hassan
+# 24 sept
+
+# chooose your own adventure story focusing
+# on using variables and branching/conditionals
+
+# Intro
+
+
+# Problem
+
+
+# Rising Action
+
+
+# Climax
+
+
+# Resolution
+greeting = "Hello, you are playing Spiderman"
+print(greeting)
+
+design_character = input("How would you want your character to look?")
+print("Excellent!!")
+print("Before you begin, you must know...")
+
+health = 200
+inventory = ("webshooters", "Spider-arms")
+
+heal_bar_full = True
+if heal_bar_full:
+    health += 20
+
+print("Game Starts")
+print("You're swinging around when your spidersense tingles")
+print("A robbery is happening nearby!")
---
+++ notes-1-intro.py
@@ -0,0 +1,57 @@
+# notes - introduction
+# 16 september
+# Thierry Narcisse
+
+# Create our owm chatbot
+
+# 1. greet the user with apredeterminted statement
+
+
+greeting = "Hello! I am Chatbot."
+
+# 2. Print out the statementpython3
+print(greeting)
+
+# 3. introduce the bot
+print("my name is meGPT.")
+print("I'm not like the other chatbots")
+print("I am completely deterministc")
+
+# 4. wow the user with some maths
+print("I bet you dont know what 8*8 is.")
+print("I can do it")
+print(f"8*8 is actually {8 * 8}.")
+
+print("what is pi squared")
+print("I can do it")
+print(f"it is {3.141592653**2}.")
+# 4. get the users name
+user_name = input(" what is your name?")
+print(f"it's nice to meet you, {user_name}.")
+
+print("I have some question for you.")
+
+print("what is 50*2 ")
+print("are you sure?")
+print(f"it's {50 * 2}.")
+
+print("next question")
+
+print("what is my favourite color")
+color = input
+print(f"It is {color}.")
+
+print("last question...")
+
+print("what is your faourite animal?")
+animal = input
+
+print(f"{animal}. oh thats cool.")
+
+want_cookies = True
+want_chips = True
+if want_cookies and want_chips:
+    print("you get both")
+
+else:
+    print("you get nothing")
