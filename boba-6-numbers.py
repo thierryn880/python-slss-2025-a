725dbf9f7261d9584660c25599fbd885cedb2265
TNTbro35 <thierryoma@icloud.com>
2025-11-05 10:29:26 -0800

    notes 6

---
+++ boba-6-numbers.py
@@ -0,0 +1,22 @@
+# author thierryn
+# date #nov 5 2025
+#
+# make a algorithm to gather data
+
+# to find most popular
+
+# bubble  tea places
+# version 1
+# show all choices
+# ask user for their choice
+# keep track with tally
+# data analysis
+# give raw score
+# give score as percentage
+#
+# version2
+# ask user for favourite boba place
+# keep tally
+# data analysis
+# give raw scores
+# give socres a s a percentage
