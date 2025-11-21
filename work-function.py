30b6459c623ad45bdf231595e16f1f06a52e5882
TNTbro35 <thierryoma@icloud.com>
2025-11-21 14:26:47 -0800

    added functions

---
+++ work-function.py
@@ -0,0 +1,11 @@
+# functions
+# author; thierryn
+# 10 october
+
+
+def average(num_one: float, num_two: float, num_three: float) -> float:
+    return 0
+
+
+results = average(41, 40, 31)
+print(results)
