289e0353d48da9b8b6b3e15620ccfa69eefec4e6
TNTbro35 <thierryoma@icloud.com>
2025-11-21 14:25:47 -0800

    added maths 1

---
+++ work-maths.py
@@ -0,0 +1,27 @@
+# thierry narcisse\
+# work-maths
+
+
+# uses f = ma
+# to find total force
+#
+#
+#
+#
+
+
+#  ask user for mass(kg) and acceleration(m/s)
+
+
+def force_cal(mass, accelration):
+    """finds force with given mass and accel"""
+
+
+print("f = m * a calculator")
+print("force(F) = 1")
+print("say 1 to start")
+
+force_you = input("1")
+
+if force_you == "1":
+    m = float(input)
