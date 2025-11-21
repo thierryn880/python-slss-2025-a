8f587a515cf78ba319d776867992ce920898b967
TNTbro35 <thierryoma@icloud.com>
2025-11-21 14:23:47 -0800

    notes7 big

--- notes-7-big-data.py
+++ notes-7-big-data.py
@@ -1,11 +1,10 @@
 # big data
 # author: Thierry Narcisse
-# nov 17 2025
+# 17 November 2025
 
-# open flies using python
-# read data in files
-# interpret the data we read
-
+# Open files using Python
+# Read the data in the files
+# Interpret the data that we read
 
 def main():
     # Get the file
@@ -14,12 +13,54 @@ def main():
     # Read the file
     header_row = file.readline()
 
-    # Read the rest of the file
+    # Get information about the fave pizza place
+    uncle_fatihs = 0
+    club_ilia = 0
+    pizza_hut = 0
+
+    # Get info for fave burrito
+    guadalupe = 0
+    quesada = 0
+
     for line in file:
-        print(line)
+        # fave pizza is in column 5 (4 index)
+        # each line is a string
+        # split the string wherever a , is
+        # convert from string -> list
+        info = line.split(",")
+        fave_pizza = info[4]
+        fave_burrito = info[5]
+
+        if fave_pizza == "Uncle Fatih's":
+            uncle_fatihs += 1
+        elif fave_pizza == "Club Ilia":
+            club_ilia += 1
+        elif fave_pizza == "Pizza Hut":
+            pizza_hut += 1
+
+        if fave_burrito == "Guadalupe (MBC)":
+            guadalupe += 1
+        elif fave_burrito == "Quesada (Cornerstone)":
+            quesada += 1
+
+    # Display results
+    print("------------------")
+    print("Results:")
+    print(f"Uncle Fatih's votes: {uncle_fatihs}")
+    print(f"Club Ilia votes: {club_ilia}")
+    print(f"Pizza Hut votes: {pizza_hut}")
+
+    if guadalupe > quesada:
+        print("Guadalupe is the most popular burrito!")
+    elif quesada > guadalupe:
+        print("Quesada wins most popular burrito!")
+    else:
+        print("It's a tie between Guadalupe and Quesada.")
+
+    print("------------------")
+
+
 
     file.close()
 
-
 if __name__ == "__main__":
-    main()
